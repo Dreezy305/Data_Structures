871afb4d201b1e13d916c3c116803b7a53092698
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 11:46:55 +0000

    added js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // document.getElementById("counter").innerHTML = 5;
-let myAge = 27;
-let humanDogRatio = 7;
+// let myAge = 27;
+// let humanDogRatio = 7;
 
-let myDogAge = myAge * humanDogRatio;
+// let myDogAge = myAge * humanDogRatio;
 
-console.log(myDogAge);
+// console.log(myDogAge);
+
+let bonusPoints = 50;
+
+bonusPoints = bonusPoints + 25;
+
+bonusPoints = bonusPoints - 25;
+
+console.log(bonusPoints);
