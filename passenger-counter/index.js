077cd0b5ecd0dd6f9d152cb016a577acba2f414d
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 13:54:04 +0000

    another commit

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,12 +1,5 @@
-// initialise the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the html and reflect the new count
-// define event listener inside the html
-
 let count_El = document.getElementById("counter");
 console.log(count_El);
-
 let count = 0;
 function increment() {
   count = count + 1;
