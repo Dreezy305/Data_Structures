06109d57286d089605ca96514c8dc3815c2b0016
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 15:01:50 +0000

    added another commit

---
+++ welcome-message/index.js
@@ -0,0 +1,7 @@
+//store the welcome-el and store it in a variable
+let WelcomeEl = document.getElementById("welcome-el");
+
+let MyName = "Idris";
+let greeting = "welcome back, How was your trip";
+
+WelcomeEl.innerHTML = Myname + greeting;
