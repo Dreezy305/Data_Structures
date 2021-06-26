d98f495ed575dce8239970be248d65b491a074d0
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 13:46:02 +0000

    added js

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -5,9 +5,11 @@
 // define event listener inside the html
 
 let count_El = document.getElementById("counter");
+console.log(count_El);
+
 let count = 0;
 function increment() {
   count = count + 1;
-  count_El.innerText = count;
+  count_El.innerHTML = count;
   console.log("button was clicked : ", count, " time(s)");
 }
