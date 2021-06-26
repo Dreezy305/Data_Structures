0dcbc83940e432bc6a70757329be38e6d95e8587
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 12:36:01 +0000

    added event

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the html and reflect the new count
+// define event listener inside the html
+
+function increment() {
+  console.log("button was clicked");
+}
