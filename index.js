665a27f87a87db8b222b8e0b9d30f2b09ad40323
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 11:23:29 +0000

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("counter").innerHTML = 5;
+// document.getElementById("counter").innerHTML = 5;
+let count = 0;
+
+console.log(count);
+
+let myAge = 27;
+
+console.log(myAge);
