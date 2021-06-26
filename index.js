73d92a9105321d2370ecbf2cd378ca3cdb8785c1
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 11:35:10 +0000

    added js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // document.getElementById("counter").innerHTML = 5;
-let count = 5 + 7;
+let first_count = 5;
+let second_count = 7;
+
+let count = first_count + second_count;
 
 console.log(count);
