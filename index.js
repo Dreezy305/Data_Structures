bc21658b5f473b173cf7c17705b3ca7a8a18a7bc
Dreezy305 <bankoleidris@gmail.com>
2021-06-26 12:31:39 +0000

    another commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// initialise the count
+// initialise the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the html and reflect the new count
