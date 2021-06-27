29ccb8d90f8e9bc475ddeba7d3197c94f54104d8
Dreezy305 <bankoleidris@gmail.com>
2021-06-27 18:41:00 +0000

    another commit

--- Black-jack/index.js
+++ Black-jack/index.js
@@ -2,7 +2,8 @@ let _name = "Linda";
 let greeting = "Hi there";
 
 function Welcome() {
-  let greetings = greeting + "," + " " + _name;
+  let greetings = greeting + "," + " " + _name + "!";
+  console.log(greetings);
 }
 
 Welcome();
