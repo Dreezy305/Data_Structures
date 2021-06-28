08ec3db660fd3d616087de2b95ceea9687be37c6
Dreezy305 <bankoleidris@gmail.com>
2021-06-28 13:01:30 +0000

    added js

--- Black-jack/index.js
+++ Black-jack/index.js
@@ -1,12 +1,14 @@
-let first_card = 6;
-let second_card = 9;
+let first_card = 10;
+let second_card = 11;
+let hasBlackJack = false;
 
 let sum = first_card + second_card;
 
-if (sum < 21) {
+if (sum <= 20) {
   console.log("Do you want to draw a new card? ");
 } else if (sum === 21) {
   console.log("You are out of the game");
+  hasBlackJack = true;
 } else {
   console.log("You are out of the game");
 }
