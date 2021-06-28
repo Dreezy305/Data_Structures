3e059c3012937f853a3655f650cc42ee170d16c5
Dreezy305 <bankoleidris@gmail.com>
2021-06-28 11:30:11 +0000

    building a gam app

--- increment-decrement/index.js
+++ increment-decrement/index.js
@@ -2,3 +2,14 @@ let myPoints = 3;
 
 // create two functions, add3points() and remove1point()
 // have them add to / remove from myPoints variable.
+
+function addToPoints() {
+  myPoints += 3;
+}
+
+function removePoints() {
+  myPoints -= 1;
+}
+
+addToPoints();
+removePoints();
