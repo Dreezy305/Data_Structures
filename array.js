8e3a98a3f12abd1ea354c3d7db353f0ad87ad341
Dreezy305 <bankoleidris@gmail.com>
2021-06-29 12:15:49 +0000

    working on arrays

--- array.js
+++ array.js
@@ -1,2 +1,8 @@
 // arrays -  ordered list of items
-let featuredPosts = ["check out my netflix", "here is the code for my project"];
+let featuredPosts = [
+  "check out my netflix",
+  "here is the code for my project",
+  "i've relaunched my portfolio",
+];
+
+let experience = [1, 2, 3, 4, 5, 6];
