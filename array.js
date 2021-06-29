d6d9972d5e7b781b22998eea06f37e9f17bcfd2a
Dreezy305 <bankoleidris@gmail.com>
2021-06-29 11:56:32 +0000

    working on array loop

--- array.js
+++ array.js
@@ -1,17 +1,1 @@
-// arrays -  ordered list of items - composite / complex data
-let featuredPosts = [
-  "check out my netflix",
-  "here is the code for my project",
-  "i've relaunched my portfolio",
-];
-
-let experience = [1, 2, 3, 4, 5, 6];
-
-let mySelf = ["idris", 20, true];
-
-let newPost = "i love tv series";
-
-featuredPosts.push(newPost);
-console.log(featuredPosts);
-featuredPosts.pop(newPost);
-console.log(featuredPosts);
+//counting in arrays
