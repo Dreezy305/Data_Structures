8a26f1cad40a90e8403abc4c07078278617df53b
Dreezy305 <bankoleidris@gmail.com>
2021-06-30 13:56:37 +0000

    added player object

--- array.js
+++ array.js
@@ -1,4 +1,10 @@
 // Objects - store data in-depth - composite/ complex data type
 //key-value pairs
 
-let course = {};
+let course = {
+  title: "css grid",
+  lessons: 16,
+  creator: "harald borgen",
+  length: "7 hours",
+  level: 3,
+};
