252618662121236b7fb4fa3332188b3cfbee1671
Dreezy305 <bankoleidris@gmail.com>
2021-06-30 14:16:06 +0000

    doing exercises

--- array.js
+++ array.js
@@ -4,16 +4,24 @@ let personObject = {
   country: "germany",
 };
 
+let large_countries = ["china", "india", "usa", "indonesia", "pakistan"];
+
 function logData() {
   person_info =
     personObject.Name +
     " is " +
     personObject.age +
     " old " +
-    "  and lives in " +
+    "and lives in " +
     personObject.country;
 
   console.log(person_info);
 }
 
+function showNations() {
+  for (let i = 0; i < large_countries.length; i++) {
+    console.log("--> " + large_countries[i]);
+  }
+}
+showNations();
 logData();
