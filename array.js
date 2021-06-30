a546869f3384f3451a932d603bda520b238510f9
Dreezy305 <bankoleidris@gmail.com>
2021-06-30 14:02:22 +0000

    added player object

--- array.js
+++ array.js
@@ -5,8 +5,10 @@ let course = {
   title: "css grid",
   lessons: 16,
   creator: "harald borgen",
-  length: "7 hours",
+  length: 73,
   level: 3,
   isFree: true,
   tags: ["html", "css", "js"],
 };
+
+console.log(course.length);
