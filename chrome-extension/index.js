a981bca17b821df95f69220bf258fe9d35e54874
Dreezy305 <bankoleidris@gmail.com>
2021-07-01 13:19:08 +0000

    building a chrome extension project

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -4,13 +4,7 @@ const myLeads = [];
 const InputEl = document.getElementById("input-el");
 
 SaveInput.addEventListener("click", function () {
-  console.log("button clicked from add event listener");
-  myLeads.push("www.awesomeleads.com");
+  console.log("button clicked");
+  myLeads.push(InputEl.value);
   console.log(myLeads);
 });
-
-// function display() {
-//   if (SaveInput.addEventListener) {
-//     InputEl.textContent = myLeads;
-//   }
-// }
