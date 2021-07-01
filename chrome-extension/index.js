7bda70f4c706492728865f751d5c9775711a250a
Dreezy305 <bankoleidris@gmail.com>
2021-07-01 13:36:26 +0000

    building a chrome extension project

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -2,6 +2,7 @@ const SaveInput = document.getElementById("input-btn");
 
 const myLeads = ["lead.com", "lead1.com", "lead2.com", "lead3.com"];
 const InputEl = document.getElementById("input-el");
+const ListEl = document.getElementById("list-el");
 
 SaveInput.addEventListener("click", function () {
   console.log("button clicked");
