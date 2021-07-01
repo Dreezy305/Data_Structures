7ae230d6a5a33c503fc1936a83a3de1708b13b71
Dreezy305 <bankoleidris@gmail.com>
2021-07-01 13:27:33 +0000

    building a chrome extension project

---
+++ chrome-extension/index.js
@@ -0,0 +1,9 @@
+let SaveInput = document.getElementById("input-btn");
+
+function Save() {
+  console.log("Button clicked");
+}
+
+SaveInput.addEventListener("click", function () {
+  console.log("button clicked add event listen");
+});
