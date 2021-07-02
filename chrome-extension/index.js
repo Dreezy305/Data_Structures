b5dced93f59cb5a60afbcee848f26b8d73cfd394
Dreezy305 <bankoleidris@gmail.com>
2021-07-02 13:33:40 +0000

    working on chrome extension

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,31 +1,29 @@
 const SaveInput = document.getElementById("input-btn");
 
-const myLeads = [
-  "www.awesomelead.com",
-  "www.epiclead.com",
-  "www.greatlead.com",
-];
+const myLeads = [];
 const InputEl = document.getElementById("input-el");
 const ListEl = document.getElementById("list-el");
+
+function renderLeads() {
+  let ListItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    ListItems += "<li>" + myLeads[i] + "</li>";
+    // ListEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    // create element
+    // set text content
+    // append to unordered list
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ListEl.append(li);
+    // console.log(ListEl.textContent);
+  }
+
+  ListEl.innerHTML = ListItems;
+}
 
 SaveInput.addEventListener("click", function () {
   console.log("button clicked");
   myLeads.push(InputEl.value);
   console.log(myLeads);
+  renderLeads();
 });
-
-let ListItems = "";
-for (let i = 0; i < myLeads.length; i++) {
-  ListItems += "<li>" + myLeads[i] + "</li>";
-
-  // ListEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-  // create element
-  // set text content
-  // append to unordered list
-  // const li = document.createElement("li");
-  // li.textContent = myLeads[i];
-  // ListEl.append(li);
-  // console.log(ListEl.textContent);
-}
-
-ListEl.innerHTML = ListItems;
