65de3b9c2c1da152ece44d2ec3835fd33c86b0b6
Dreezy305 <bankoleidris@gmail.com>
2021-07-02 13:37:29 +0000

    cleaing up the code

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -8,14 +8,6 @@ function renderLeads() {
   let ListItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     ListItems += "<li>" + myLeads[i] + "</li>";
-    // ListEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    // create element
-    // set text content
-    // append to unordered list
-    // const li = document.createElement("li");
-    // li.textContent = myLeads[i];
-    // ListEl.append(li);
-    // console.log(ListEl.textContent);
   }
 
   ListEl.innerHTML = ListItems;
