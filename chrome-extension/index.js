77349c4f7a12e36006a44e033ecc40099dfcb0d0
Dreezy305 <bankoleidris@gmail.com>
2021-07-02 14:37:18 +0000

    cleaing up the code

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -7,12 +7,16 @@ let ListEl = document.getElementById("list-el");
 function renderLeads() {
   let ListItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    ListItems +=
-      "<li><a href='" +
-      myLeads[i] +
-      "' target='_blank'>" +
-      myLeads[i] +
-      "</a></li>";
+    // ListItems +=
+    //   "<li><a href='" +
+    //   myLeads[i] +
+    //   "' target='_blank'>" +
+    //   myLeads[i] +
+    //   "</a></li>";
+    ListItems += `
+          <li>
+              <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+          </li>`;
 
     console.log(ListItems);
   }
