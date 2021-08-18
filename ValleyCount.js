49f0deb52c75f022d255ecbdc385a3c70922d951
Dreezy305 <bankoleidris@gmail.com>
2021-08-18 20:26:26 +0000

    valley count

---
+++ ValleyCount.js
@@ -0,0 +1,22 @@
+function ValleyCount(n, steps) {
+  steps = "";
+  let step = steps.split(",");
+  console.log(step);
+  let currentSeaLevel = 0;
+  let valleyCount = 0;
+  let valley = false;
+
+  step.forEach((i) => {
+    i === "U" ? currentSeaLevel++ : currentSeaLevel--;
+    if (currentSeaLevel < 0 && !valley) {
+      valleyCount++;
+      valley = true;
+    } else if (currentSeaLevel >= 0 && valley) {
+      valley = false;
+    }
+  });
+  return valleyCount;
+}
+
+console.log(ValleyCount(8, "U,D,D,D,U,D,U,U"));
+console.log(ValleyCount(12, "D,D,U,U,D,D,U,D,U,U,U,D"));
