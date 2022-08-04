a0721b9c7384bf22911e288007127966e4e66f64
Dreezy <bankoleidris@gmail.com>
2022-08-04 18:50:40 +0000

    counting apples

---
+++ Hackerrank/counting-apples/index.js
@@ -0,0 +1,26 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  const iteratorA = apples.keys();
+  const iteratorB = oranges.keys();
+  let resultA = [];
+  let resultB = [];
+  for (const key of iteratorA) {
+    let addA = a + apples[key];
+    if (addA >= s && addA <= t) {
+      resultA.push(addA);
+    }
+  }
+
+  for (const key of iteratorB) {
+    let addB = b + oranges[key];
+    if (addB >= s && addB <= t) {
+      resultB.push(addB);
+    }
+  }
+
+  console.log(resultA.length);
+  console.log(resultB.length);
+
+  return [resultA.length, resultB.length];
+}
+
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
