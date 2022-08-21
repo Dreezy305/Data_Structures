b07d6558c1398cf83815642914975e8f169820bb
Dreezy <bankoleidris@gmail.com>
2022-08-21 15:27:29 +0000

    rotate left

---
+++ Hackerrank/minimum-bribes/index.js
@@ -0,0 +1,19 @@
+function minimumBribes(queue) {
+  let chaotic = false;
+  var bribes = 0;
+  for (let i = 0; i < queue.length; i++) {
+    if (queue[i] - (i + 1) > 2) {
+      chaotic = true;
+    }
+    for (let j = queue[i] - 2; j < i; j++) {
+      if (queue[j] > queue[i]) {
+        bribes++;
+      }
+    }
+  }
+  if (chaotic === true) {
+    console.log("Too chaotic");
+  } else {
+    console.log(bribes);
+  }
+}
