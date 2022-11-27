a04044a6def35b1dc77660c18d12e5a51ac301d6
Dreezy305 <bankoleidris@gmail.com>
2022-11-27 08:45:15 +0100

    numbe rline jumps

---
+++ Hackerrank/number-line-jumps/index.js
@@ -0,0 +1,17 @@
+function kangaroo(x1, v1, x2, v2) {
+  let result = "NO";
+
+  if (v1 <= v2) return result;
+
+  while (x1 <= x2) {
+    x1 += v1;
+    x2 += v2;
+
+    if (x1 === x2) {
+      result = "YES";
+      break;
+    }
+  }
+
+  return result;
+}
