b581363b26ee3c94dafaf5ae7d5992d4a35c66a7
idris-simplify <156663919+idris-simplify@users.noreply.github.com>
2026-02-14 11:37:18 +0100

    fixed valid parenthesis

---
+++ Leetcode/20.js
@@ -0,0 +1,30 @@
+function isValid(s) {
+  let arrStack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      arrStack.push(s[i]);
+    } else {
+      if (arrStack.length === 0) {
+        return false;
+      }
+      let top = arrStack.pop();
+      if (s[i] === ")" && top !== "(") {
+        return false;
+      }
+      if (s[i] === "}" && top !== "{") {
+        return false;
+      }
+      if (s[i] === "]" && top !== "[") {
+        return false;
+      }
+      if (s[i] === "(" && top === "(") {
+        return false;
+      }
+    }
+  }
+
+  if (s.length === 1 || arrStack.length > 0) {
+    return false;
+  }
+  return true;
+}
